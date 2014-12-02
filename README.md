939e1868e7312531f8783d651b17929c9a855ddd
rsgfernandes <rsgfernandes@gmail.com>
2014-12-02 16:27:33 -0200

    Zabbix AWS SES notification Script
    Version 1

---
+++ README.md
@@ -0,0 +1,12 @@
+description:
+============
+
+script for zabbix send e-mails notifications using AWS SES.
+
+
+usage:
+======
+
+1. save it at zabbix alert scripts folder
+2. create a new media type
+3. create a new action with AND/OR condition
---
+++ zabbix-ses.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script parameters
+DESTINO="$1"
+DE="digite seu e-mail"
+ASSUNTO="$2"
+MENSAGEM="$3"
+
+# Validate script syntax
+[ $# -eq 0 ] && { echo "Usage: $0 e-mail@address.com subject message"; exit 1; }
+
+# Set cURL variables
+date="$(date -R)"
+access_key="digite sua access key"
+secret_key="digite sua secret key"
+assinatura="$(echo -n "$date" | openssl dgst -sha256 -hmac "$secret_key" -binary | base64 -w 0)"
+
+# Generates Auth Cert
+cabecalho_autenticacao="X-Amzn-Authorization: AWS3-HTTPS AWSAccessKeyId=$access_key, Algorithm=HmacSHA256, signature=$assinatura"
+
+# AWS SES Endpoint - Change it for your prefered region
+endpoint="https://email.us-east-1.amazonaws.com/"
+
+# AWS SES Calls
+acao_envio="Action=SendEmail"
+remetente="Source=$DE"
+destinatario="Destination.ToAddresses.member.1=$DESTINO"
+assunto="Message.Subject.Data=$ASSUNTO"
+corpo_email="Message.Body.Text.Data=$MENSAGEM"
+
+# SES POST
+curl -v -X POST -H "Date: $date" -H "$cabecalho_autenticacao" --data-urlencode "$corpo_email" --data-urlencode "$destinatario" --data-urlencode "$remetente" --data-urlencode "$acao_envio" --data-urlencode "$assunto"  "$endpoint"
